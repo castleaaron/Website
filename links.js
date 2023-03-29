1ef71f9785de2eddde4f225ce3eefae4ea1c8879
castleaaron <80482012+castleaaron@users.noreply.github.com>
2023-03-28 20:42:10 -0500

    Delete links.js

--- links.js
+++
@@ -1,9 +0,0 @@
-document.write('\
-\
-\<a href = "resume.html"><img src = "https://www.nicepng.com/png/detail/204-2042369_download-resume-icon-transparent-background.png" id = "resume" alt = "resume"><\a>\
-\
-\<a href = "https://github.com/castleaaron"><img src = "https://thurrott.s3.amazonaws.com/wp-content/uploads/sites/2/2023/01/GitHub.jpeg" id = "github" alt = "github"></a>\
-\
-\<a href = "https://www.linkedin.com/in/aaron-castle-850175269/"><img src = "https://blog.linkedin.com/apps/settings/wcm/designs/linkedin/katy/global/clientlibs/resources/img/default-share.png" id = "linkedin" alt = "linkedin"></a>\
-\
-');
